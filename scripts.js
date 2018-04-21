55df229c982aa352d4c828ff911fe6cf328585b8
Fran James <franjonequin@gmail.com>
2018-04-21 14:25:13 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,10 @@
-(function ($) {
-    $(function () {
-
-        //initialize all modals           
-        $('.modal').modal();
-
-        //now you can open modal from code
-        $('#modal1').modal('open');
-
-        //or by click on trigger
-        $('.trigger-modal').modal();
-
-    }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function() {
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  /*$('.view').click(function (){
+  	$('#modal1').modal('open');	
+  	alert('edskjcxnm');
+  });*/
+  /*$('.view').leanModal();*/
+  $('#modal1').modal();
+  
+});
