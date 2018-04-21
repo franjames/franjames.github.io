0fd0ea45b45c38dbac0884ef4089fc580468a1f2
Fran James <franjonequin@gmail.com>
2018-04-21 14:19:54 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,4 @@
-
-  $(document).ready(function(){
-    $('.modal1').modal();
-    instance.open();
-    instance.close();
-    instance.destroy();
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal1').leanModal();
   });
